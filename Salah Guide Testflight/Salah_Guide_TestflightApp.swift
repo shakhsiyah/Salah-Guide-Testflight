34cc6cfc9f61119f62a74a6ae4592aae43c4fd4a
shakhsiyah <shams@securiwiser.com>
2024-10-24 11:27:19 +0100

    Initial Commit

---
+++ Salah Guide Testflight/Salah_Guide_TestflightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Salah_Guide_TestflightApp.swift
+//  Salah Guide Testflight
+//
+//  Created by Shamsul Islam on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Salah_Guide_TestflightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Salah Guide TestflightTests/Salah_Guide_TestflightTests.swift
@@ -0,0 +1,17 @@
+//
+//  Salah_Guide_TestflightTests.swift
+//  Salah Guide TestflightTests
+//
+//  Created by Shamsul Islam on 24/10/2024.
+//
+
+import Testing
+@testable import Salah_Guide_Testflight
+
+struct Salah_Guide_TestflightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
